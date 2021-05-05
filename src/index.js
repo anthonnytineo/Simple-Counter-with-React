0336dfa775e168be69f77955af0b6722796e61f7
anthonny tineo <anthonny@MacBook-Pro-de-anthonny.local>
2021-05-04 23:13:57 -0400

    comentarios

--- src/index.js
+++ src/index.js
@@ -13,26 +13,27 @@ let num5 = 0;
 let num6 = 0;
 
 setInterval(() => {
+// Incio del contador y contador de milecimas de segundos
     if(num6++, num6 === 100){
         num5++;
         num6 = 0;
     }
-
+// contador de segundos
     if(num5 === 60){
         num4++;
         num5 = 0;
     }
-
+// contador de minutos
     if(num4 === 60){
         num3++;
         num4 = 0;
     }
-
+// contador de dias
     if(num3 === 24){
         num2++;
         num3 = 0;
     }
-    
+// contador de semanas
     if(num2 === 7){
         num1++;
         num2 = 0;
